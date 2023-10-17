8210ad72803daa16ca8181289fc24aaafff3cb3d
tenfoldpaper <s.bien@tum.de>
2023-10-17 16:21:22 +0200

    formatting

--- franka_hardware/include/franka_hardware/helper_functions.hpp
+++ franka_hardware/include/franka_hardware/helper_functions.hpp
@@ -9,6 +9,6 @@
 
 // A dump of functions that are used by both hardware and multi_hardware interfaces.
 namespace franka_hardware {
-bool all_of_element_has_string(std::vector<std::string> vec, std::string content);
-int check_command_mode_type(std::vector<std::string> interfaces);
+  bool all_of_element_has_string(std::vector<std::string> vec, std::string content);
+  int check_command_mode_type(std::vector<std::string> interfaces);
 }
